eae52ba810b9e9743562f96222575a0ca8c0f8ee
yuta322@gmail.com <yuta322@gmail.com>
2023-06-09 00:55:18 +0900

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,32 @@
-// import { MiddlewareRequest, NextRequest } from "@netlify/next";
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-const PUBLIC_FILE = /\.(.*)$/;
+let locales = ["en-US", "nl-NL", "nl"];
 
-export async function middleware(req: NextRequest) {
-  try {
-    // const req = new MiddlewareRequest(nextRequest);
+// Get the preferred locale, similar to above or using a library
+export function middleware(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
+  const pathname = request.nextUrl.pathname;
+  const pathnameIsMissingLocale = locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  );
 
-    if (
-      req.nextUrl.pathname.startsWith("/_next") ||
-      req.nextUrl.pathname.includes("/api/") ||
-      PUBLIC_FILE.test(req.nextUrl.pathname)
-    ) {
-      return;
-    }
+  // Redirect if there is no locale
+  if (pathnameIsMissingLocale) {
+    const defaultLocale = "en";
 
-    if (req.nextUrl.locale === "default") {
-      const locale = req.cookies.get("NEXT_LOCALE")?.value || "en";
-
-      return NextResponse.redirect(
-        new URL(
-          `/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`,
-          req.url
-        )
-      );
-    }
-  } catch (error) {
-    console.log(error);
+    // e.g. incoming request is /products
+    // The new URL is now /en-US/products
+    return NextResponse.redirect(
+      new URL(`/${defaultLocale}/${pathname}`, request.url)
+    );
   }
 }
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    "/((?!_next).*)",
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+};
