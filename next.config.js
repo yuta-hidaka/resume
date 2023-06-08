9f1c057b227d2ac61e2a374290223363863f4d2e
yuta322@gmail.com <yuta322@gmail.com>
2023-06-09 01:16:35 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    i18n: {
-      locales: ['en', 'ja'],
-      defaultLocale: 'en',
-    },
+    // i18n: {
+    //   locales: ['en', 'ja'],
+    //   defaultLocale: 'en',
+    // },
   };
