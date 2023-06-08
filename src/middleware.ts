70397a9f12d35cdeceb95a2eddd2c396581517bc
yuta322@gmail.com <yuta322@gmail.com>
2023-06-09 00:37:09 +0900

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,21 +4,28 @@ import { NextResponse } from "next/server";
 const PUBLIC_FILE = /\.(.*)$/;
 
 export async function middleware(nextRequest: NextRequest) {
-  const req = new MiddlewareRequest(nextRequest);
+  try {
+    const req = new MiddlewareRequest(nextRequest);
 
-  if (
-    req.nextUrl.pathname.startsWith("/_next") ||
-    req.nextUrl.pathname.includes("/api/") ||
-    PUBLIC_FILE.test(req.nextUrl.pathname)
-  ) {
-    return;
-  }
+    if (
+      req.nextUrl.pathname.startsWith("/_next") ||
+      req.nextUrl.pathname.includes("/api/") ||
+      PUBLIC_FILE.test(req.nextUrl.pathname)
+    ) {
+      return;
+    }
 
-  if (req.nextUrl.locale === "default") {
-    const locale = req.cookies.get("NEXT_LOCALE")?.value || "en";
+    if (req.nextUrl.locale === "default") {
+      const locale = req.cookies.get("NEXT_LOCALE")?.value || "en";
 
-    return NextResponse.redirect(
-      new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
-    );
+      return NextResponse.redirect(
+        new URL(
+          `/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`,
+          req.url
+        )
+      );
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
